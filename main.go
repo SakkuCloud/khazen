ac649421e6997452c53b94857badd5ab0334673d
Seyedmajid Etehadi <ettehadi.m@gmail.com>
2019-11-26 12:56:47 +0330

    Add main

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"github.com/evalphobia/logrus_sentry"
+	"github.com/jinzhu/configor"
+	log "github.com/sirupsen/logrus"
+	"khazen/app"
+	"khazen/config"
+	"os"
+	"time"
+)
+
+func main() {
+	var debugMode = flag.Bool("debug", false, "run in debug mode")
+	var configFile = flag.String("c", "/etc/khazen/config.yml", "config file location (must be yaml format)")
+	flag.Parse()
+
+	// log
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	// config file
+	err := configor.Load(&config.Config, *configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// log file
+	logFile, err := os.OpenFile(config.Config.LogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.Infof("Logging to %s", config.Config.LogFile)
+		log.SetOutput(logFile)
+	} else {
+		log.Info("Failed to log to file, using default")
+	}
+
+	// sentry
+	if config.Config.SentryDSN != "" {
+		hook, _ := logrus_sentry.NewSentryHook(config.Config.SentryDSN, []log.Level{
+			log.PanicLevel,
+			log.FatalLevel,
+			log.ErrorLevel,
+			log.WarnLevel,
+		})
+		hook.Timeout = config.SentryTimeout * time.Second
+		log.AddHook(hook)
+	}
+
+	// debug mode
+	if *debugMode {
+		log.Infoln("Log in debug mode!")
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+
+	// run app
+	api := &app.App{}
+	api.Init()
+	api.Run(":" + config.Config.Port)
+}
